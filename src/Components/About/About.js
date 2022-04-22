d708087f12d220d57256758633a9f43abfcdf720
idilipsarker <dilipsarker719@gmail.com>
2022-04-22 12:51:05 +0600

    blogs first question added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div className='about'>
             <img src={myPhoto} alt=""></img> 
-            <p><u>My Goal:</u> My goal is very simple. I want to learn Full Stack Web-Development in 2022. I want to learn Node.js and other technologies to become a full stack web-developer. Then I want to get a job. Afterwards, in future I want to learn  Python and Golang.That's my simple goal.</p>
+            <p><u>My Goal:</u> My goal is very simple. I want to learn Full Stack Web-Development in 2022. I want to learn Node.js and other technologies to become a full stack web-developer. Then I want to get a job. Afterwards, in future I want to learn  Python and Golang.That's my simple goal. I will try my best to archive this goal.</p>
         </div>
     );
 };
--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import "./Blog.css"
 const Blog = () => {
     return (
-        <div>
-          <h3>Blog section added</h3>  
-        </div>
+      <div className='blog'>
+        <h3>1. Difference between authorization and authentication?</h3>
+        <p>
+          Answer:{" "}
+          <p>
+            1. Authentication confirms users identity to grant access to the
+            system. On the other hand, Authorization determines whether users
+            are authorized to access the resources.
+          </p>
+          <p>
+            2. Authentication is the process of validating user credentials to
+            gain user access. On the other hand, Authorization is the process of
+            verifying whether access is allowed or not.
+          </p>
+          <p>
+            3. Authentication is the process of verifying whether access is
+            allowed or not. On the other hand, Authorization determines what
+            user can and cannot access.
+          </p>
+          <p>
+            4. Authentication usually requires a username and a password. On the
+            other hand, Authentication factors required for authorization may
+            vary, depending on the security level.
+          </p>
+          <p>
+            5. Authentication is the first step of authorization so always comes
+            first. On the other hand, Authorization is done after successful
+            authentication.
+          </p>
+        </p>
+      </div>
     );
 };
 
