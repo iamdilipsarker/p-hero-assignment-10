9351818894cf7666af85b8f527f2784c3ce9e56e
idilipsarker <dilipsarker719@gmail.com>
2022-04-22 14:29:50 +0600

    register form created

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Blog.css"
 const Blog = () => {
     return (
-      <div className='blog'>
+      <div className="blog">
         <h3>1. Difference between authorization and authentication?</h3>
         <p>
           Answer:{" "}
@@ -30,6 +30,45 @@ const Blog = () => {
             5. Authentication is the first step of authorization so always comes
             first. On the other hand, Authorization is done after successful
             authentication.
+          </p>
+        </p>
+        <h3>
+          2. Why are you using firebase? What other options do you have to
+          implement authentication?
+        </h3>
+        <p>
+          Answer: We use firebase because, Firebase Authentication provides
+          backend services, easy-to-use SDKs, and ready-made UI libraries to
+          authenticate users to your app. It supports authentication using
+          passwords, phone numbers, popular federated identity providers like
+          Google, Facebook and Twitter, and more. Firebase Authentication
+          integrates tightly with other Firebase services, and it leverages
+          industry standards like OAuth 2.0 and OpenID Connect, so it can be
+          easily integrated with your custom backend.
+        </p>
+        <p>
+          Other option we have to implements authentication: We can use Octa,
+          OAuth, Heroku,Parse,Back4App etc.{" "}
+        </p>
+
+        <h3>
+          3. What other services does firebase provide other than authentication?
+        </h3>
+        <p>
+          Answer: Firebase is originally developed by Firebase inc and later
+          acquired by Google. It provides different kinds of services that help
+          you to develop high-quality mobile and web applications to grow your
+          business. It is compatible with Web, iOS, Android, and OS X clients.
+          With Firebase, developers don't need to worry about the backend
+          programming, Authentication, API development, database (real-time
+          cloud-hosted NoSQL database and cloud firestore), File storage, etc.
+          Firebase provides all the services with very efficient and fast
+          performance.
+          <p>
+            There are many services which Firebase provides, Most useful of them
+            are: Cloud Firestore, Cloud Functions, Authentication, Hosting,
+            Cloud Storage, Google Analytics, Predictions, Cloud Messaging,
+            Dynamic Links, Remote Config.
           </p>
         </p>
       </div>
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
+      <div className="form-container">
         <div>
-            
+          <h3 className="form-title">Please Register</h3>
+          <form>
+            <div className="form-input-group">
+              <label htmlFor="email">Email</label>
+              <input
+                required
+                type="email"
+                name="email"
+                id=""
+                placeholder="Enter your Email"
+              />
+            </div>
+            <div className="form-input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                required
+                type="password"
+                name="password"
+                id=""
+                placeholder="Enter your Password"
+              />
+            </div>
+            <div className="form-input-group">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                required
+                type="password"
+                name="confirm-password"
+                id=""
+                placeholder="Enter your Password"
+              />
+            </div>
+            <input className="form-submit" type="submit" value="Register" />
+          </form>
+          <p>
+            Already Have an Account?{" "}
+            <Link className="form-link" to="/login">
+              Login
+            </Link>
+          </p>
         </div>
+      </div>
     );
 };
 
