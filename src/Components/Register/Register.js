ebc2c00a561f8e87605762392b629f11c5eb59fa
idilipsarker <dilipsarker719@gmail.com>
2022-04-25 11:27:42 +0600

    email verification implemented

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const navigate = useNavigate()
 
   const [CreateUserWithEmailAndPassword, user , loading, gotError] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
   
   const handleEmailBlur = event => {
     setEmail(event.target.value);
