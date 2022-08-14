dec5b7aa43fe6a79da4936f332bb40bae05af461
idilipsarker <dilipsarker719@gmail.com>
2022-08-14 22:36:14 +0600

    code changed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +11,7 @@ const firebaseConfig = {
   projectId: "dilips-p-hero-assignment-10",
   storageBucket: "dilips-p-hero-assignment-10.appspot.com",
   messagingSenderId: "872429595703",
-  appId: "1:872429595703:web:9fa65ae9c8265089dce3b2",
+  appId: "1:872429595703:web:9fa65ae9c8265089dce3b2"
 };
 
 // Initialize Firebase
